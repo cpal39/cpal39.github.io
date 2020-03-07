8caa699d195d279d5fe33f246607230b69887f74
Chris Paldino <39720561+cpal39@users.noreply.github.com>
2020-03-06 20:57:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(window).scroll(function() {
+	if ($(document).scrollTop() < 50) {
+		$('nav').removeClass('bg-blue');
+		$('nav').addClass('bg-black');
+	} else {
+		$('nav').removeClass('bg-black');
+		$('nav').addClass('bg-blue');
+	}
+});
+
+$(document).ready(function(){
+	$(".navbar-brand").click(function(e){
+		e.preventDefault();
+		let id=$(this).attr('href');
+		$("html,body").animate({
+			scrollTop: 0
+		}, 'slow');
+	});
+	$("a.nav-link").each(function(){
+		$(this).click(function(e){
+			e.preventDefault();
+			let id=$(this).attr('href');
+			$("html,body").animate({
+				scrollTop: $(id).offset().top-80
+			}, 'slow');
+		});
+	});
+});
